ee5a2b6174e475e1d10c8fda46686881ac9f7a7b
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-12 19:31:48 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.692fe122e6ce2a5ea419fbbe6dd696bd.js"
 );
 
 self.addEventListener('message', (event) => {
